e94a01024e9fda1ca1e91e186f131c8c8c7dc45c
Panzer <danieldk8@gmail.com>
2022-09-13 21:44:26 -0300

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+
+export const getGifs = async( category ) => {
+
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=62eKusxeBxU6xPpMck0jEoha5sc7IqiH&q=${ category }&limit=10`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img =>({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+
+    }))
+
+    return gifs;
+}
